796e375ac702df63bba260507601c1250ba95aea
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2023-09-14 19:31:41 +0700

    refactor
    ui implemented

--- lib/main.dart
+++ lib/main.dart
@@ -8,81 +8,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter',
+      title: 'Wista Bandung',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(),
+      home: const DetailScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Screen'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.search, color: Colors.white),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Container(
-        color: Colors.black,
-        margin: const EdgeInsets.only(top: 20),
-        child: const Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Row(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 16.0),
+              child: const Text(
+                'Farm House Lembang',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Icon(
-                    Icons.share,
-                    color: Colors.blue,
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.calendar_today),
+                      SizedBox(height: 8.0),
+                      Text('Open Everyday'),
+                    ],
                   ),
-                  Icon(
-                    Icons.thumb_up,
-                    color: Colors.blue,
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.alarm_on),
+                      SizedBox(height: 8.0),
+                      Text('09.00 - 20.00'),
+                    ],
                   ),
-                  Icon(
-                    Icons.thumb_down,
-                    color: Colors.blue,
-                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.monetization_on),
+                      SizedBox(height: 8.0),
+                      Text('Rp. 25.000'),
+                    ],
+                  )
                 ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Icon(
-                    Icons.share,
-                    color: Colors.blue,
-                  ),
-                  Icon(
-                    Icons.thumb_up,
-                    color: Colors.blue,
-                  ),
-                  Icon(
-                    Icons.thumb_down,
-                    color: Colors.blue,
-                  ),
-                ],
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0),
               ),
-              Icon(
-                Icons.share,
-                color: Colors.blue,
-              ),
-              Icon(
-                Icons.thumb_up,
-                color: Colors.blue,
-              ),
-              Icon(
-                Icons.thumb_down,
-                color: Colors.blue,
-              ),
-            ]),
+            ),
+          ],
+        ),
       ),
     );
   }
