8aaa9b1e20bbec399f080f4754c45edd0ad7e23c
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2023-09-14 20:12:20 +0700

    refactor
    dropdownmenu

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+// void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+ 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(),
-    );
-  }
+  State<FirstScreen> createState() => _FirstScreenState();
 }
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
+ 
+class _FirstScreenState extends State<FirstScreen> {
+  String? language;
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('First Screen'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.search, color: Colors.white),
-            onPressed: () {},
+      ),
+      body: DropdownButton<String>(
+        items: const <DropdownMenuItem<String>>[
+          DropdownMenuItem<String>(
+            value: 'Dart',
+            child: Text('Dart'),
+          ),
+          DropdownMenuItem<String>(
+            value: 'Kotlin',
+            child: Text('Kotlin'),
+          ),
+          DropdownMenuItem<String>(
+            value: 'Swift',
+            child: Text('Swift'),
           ),
         ],
-      ),
-      body: Container(
-        color: Colors.black,
-        margin: const EdgeInsets.only(top: 20),
-        child: const Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Icon(
-                    Icons.share,
-                    color: Colors.blue,
-                  ),
-                  Icon(
-                    Icons.thumb_up,
-                    color: Colors.blue,
-                  ),
-                  Icon(
-                    Icons.thumb_down,
-                    color: Colors.blue,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Icon(
-                    Icons.share,
-                    color: Colors.blue,
-                  ),
-                  Icon(
-                    Icons.thumb_up,
-                    color: Colors.blue,
-                  ),
-                  Icon(
-                    Icons.thumb_down,
-                    color: Colors.blue,
-                  ),
-                ],
-              ),
-              Icon(
-                Icons.share,
-                color: Colors.blue,
-              ),
-              Icon(
-                Icons.thumb_up,
-                color: Colors.blue,
-              ),
-              Icon(
-                Icons.thumb_down,
-                color: Colors.blue,
-              ),
-            ]),
+        value: language,
+        hint: const Text('Select Language'),
+        onChanged: (String? value) {
+          setState(() {
+            language = value;
+          });
+        },
       ),
     );
   }
