f8720b673ed7befd215ee52ad3c6dc716a88d08c
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2023-09-16 17:07:13 +0700

    refactor
    text filled

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,51 @@
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [DropdownButton].
+class ObscuredTextFieldSample extends StatelessWidget {
+  const ObscuredTextFieldSample({super.key});
 
-const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 250,
+      height: 500,
+      child: Column(
+        children: [
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Password',
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Password',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-void main() => runApp(const DropdownButtonApp());
-
-class DropdownButtonApp extends StatelessWidget {
-  const DropdownButtonApp({super.key});
-
+class TextFieldExampleApp extends StatelessWidget {
+  const TextFieldExampleApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('DropdownButton Sample')),
+        appBar: AppBar(title: const Text('Obscured Textfield')),
         body: const Center(
-          child: DropdownButtonExample(),
+          child: ObscuredTextFieldSample(),
         ),
       ),
     );
   }
 }
 
-class DropdownButtonExample extends StatefulWidget {
-  const DropdownButtonExample({super.key});
-
-  @override
-  State<DropdownButtonExample> createState() => _DropdownButtonExampleState();
-}
-
-class _DropdownButtonExampleState extends State<DropdownButtonExample> {
-  String dropdownValue = list.first;
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? value) {
-        // This is called when the user selects an item.
-        setState(() {
-          dropdownValue = value!;
-        });
-      },
-      items: list.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
-  }
-}
+void main() => runApp(const TextFieldExampleApp());
