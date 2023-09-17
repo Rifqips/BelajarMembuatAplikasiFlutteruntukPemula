e50235638a30cd8f07432fe8b37fa302a5fc5c20
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2023-09-17 14:35:23 +0700

    refactor
    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,65 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
-class ObscuredTextFieldSample extends StatelessWidget {
-  const ObscuredTextFieldSample({super.key});
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  final String message = 'Hello From First Screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Pindah Screen"),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SecondScreen(message);
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  final String message;
+  const SecondScreen(this.message, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(
-              children: [
-                ExpandedWidget(),
-                FlexibleWidget(),
-              ],
-            ),
-            Row(
-              children: [
-                ExpandedWidget(),
-                ExpandedWidget(),
-              ],
-            ),
-            Row(
-              children: [
-                FlexibleWidget(),
-                FlexibleWidget(),
-              ],
-            ),
-            Row(
-              children: [
-                FlexibleWidget(),
-                ExpandedWidget(),
-              ],
+            Text(message),
+            OutlinedButton(
+              child: const Text('Kembali'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             )
           ],
         ),
@@ -40,64 +67,3 @@ class ObscuredTextFieldSample extends StatelessWidget {
     );
   }
 }
-
-class ExpandedWidget extends StatelessWidget {
-  const ExpandedWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.teal, border: Border.all(color: Colors.white)),
-        child: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Text(
-            'Expanded',
-            style: TextStyle(color: Colors.white, fontSize: 24),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class FlexibleWidget extends StatelessWidget {
-  const FlexibleWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Flexible(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.tealAccent,
-          border: Border.all(color: Colors.white),
-        ),
-        child: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Text(
-            'Flexible',
-            style: TextStyle(color: Colors.teal, fontSize: 24),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TextFieldExampleApp extends StatelessWidget {
-  const TextFieldExampleApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Obscured Textfield')),
-        body: Container(
-          child: ObscuredTextFieldSample(),
-        ),
-      ),
-    );
-  }
-}
-
-void main() => runApp(const TextFieldExampleApp());
